84354ede21208e82abae269180beffc9fd960917
FelixLiu-SF <felixkat@gmail.com>
2018-01-30 16:49:18 -0800

    more blinding work, separated code to multiple files

--- Blind_New_XRs_ALL.m
+++ Blind_New_XRs_ALL.m
@@ -66,151 +66,26 @@ if(size(x_unprocessed,1)>0)
     if(~isempty(chk_oldcohort) && isempty(chk_newcohort))
       %% OLD cohort participant, do not screen
 
-      % generate barcodes & UIDs
-      newstudyuid = dicomuid;
+      % iterate the accession number counter for QC
       accnum_qc = accnum_qc+1;
-      tmpacc1 = zerofillstr(accnum_qc,4);
-      tmpacc1 = horzcat('6',tmpacc1);
 
-      for jx_se = 1:size(loop_series,1) %loop through each XR view type in exam
-
-        % collect all files for this XR view type
-        this_se = loop_series{jx_se,1};
-        all_se = indcfind(tmpstudy(:,6),horzcat('^',this_se,'$'),'regexpi');
-
-        % collect files for blinding under this XR type
-        tmpseries = tmpstudy(all_se,:);
-
-        % categorize PA beam angles
-        if(strcmpi(this_se,'PA'))
-          [tmpseries] = Relabel_XR_PA_Views_Deploy(tmpseries);
-        end %beam angle
-
-        % add unknown/empty series to the PA series
-        if(strmpci(this_se,'PA'))
-          tmpseries = [tmpseries; tmpseries(unknown_series,:); tmpseries(empty_series,:)];
-        end %add unknowns
-
-        % look up series number for this XR type
-        switch this_se
-          case 'PA'
-            tmpse=1;
-          case 'PA05'
-            tmpse=3;
-          case 'PA10'
-            tmpse=1;
-          case 'PA15'
-            tmpse=2;
-          case 'LLAT'
-            tmpse=4;
-          case 'RLAT'
-            tmpse=5;
-          case 'Full Limb'
-            tmpse=6;
-          case 'Unstitched Pelvis'
-            tmpse=7;
-          case 'Unstitched Knee'
-            tmpse=7;
-          case 'Unstitched Ankle'
-            tmpse=7;
-          otherwise
-            tmpse=7;
-        end
-
-        % generate series qc barcode
-        tmpacc2 = horzcat(tmpacc1,num2str(tmpse));
-
-        % generate output directory for this series
-        newdir =  horzcat(dcmdir_out,tmpid,'_',tmpname,'\',tmpacc2);
-        if(~exist(newdir))
-          mkdir(newdir);
-        end
-
-        %% MISSING CODE FOR GRABBING PRIOR VISIT XRAY IMAGES %%
-
-        %% blind XR images
-        for fx=1:size(tmpseries,1)
-
-          % get metadata for file
-          tmpf =    tmpseries{fx,1};
-          tmpdesc = tmpseries{fx,6};
-          tmpinfo = dicominfo(tmpf);
-
-          tmpacc3 = horzcat(tmpacc2,zerofillstr(fx,2));
-          new_desc = horzcat(tmpdesc,' ',tmpacc3);
-
-          % copy file to blinding destination
-          newf = horzcat(newdir,'\',tmpacc3);
-
-          if(~exist(newf))
-              copyfile(tmpf,newf,'f');
-          else
-              disp('Filename already exists!');
-              disp(newf);
-          end
-
-          % decompress JPEG
-          [status,result] = dcmdjpeg(newf,newf);
-          if(status~=0)
-            disp('decompression error!');
-            disp(result);
-            disp(newf);
-          end
-
-          % anonymize blinding file
-          try
-          dicomanon(newf,newf,'keep',{'PatientID','PatientName','AccessionNumber','SeriesNumber','StudyDescription','SeriesDescription','StudyID','StudyInstanceUID','SeriesInstanceUID','SOPInstanceUID'});
-          catch anonerr
-              disp(anonerr);
-              err_img = dicomread(tmpf);
-              dicomwrite(err_img,newf);
-          end
-
-          % blind XR with blinding info
-          tagcell = {...
-          '(0010,0010)',horzcat(tmpname,'^^^^'),'i';...
-          '(0010,0020)',tmpid,'i';...
-          '(0008,0050)',tmpacc2,'i';...
-          '(0020,0011)',zerofillstr(fx,2),'i';...
-          '(0008,1030)',tmpacc2,'i';...
-          '(0008,103E)',new_desc,'i';...
-          '(0020,000D)',newstudyuid,'i';...
-          '(0020,0010)',tmpacc3,'i';...
-          '(0008,0018)',tmpinfo.SOPInstanceUID,'i';...
-          '','','imt';...
-          };
-
-          [status,result] = dcmbatchmod(newf,tagcell);
-          try
-              [status,result] = FixXR(newf,tmpinfo);
-          catch
-              try
-                  pause(15);
-                  [status,result] = FixXR(newf,tmpinfo);
-              catch
-                  status = -1;
-              end
-          end
-          if(status(1)~=0)
-              disp('photometric error!');
-              disp(newf);
-          end
-
-          % re-compress JPEG
-          [status,result] = dcmcjpeg(newf,newf);
-          if(status~=0)
-            disp('compression error!');
-            disp(result);
-            disp(newf);
-          end
-
-        end %fx
-
-      end %loop_series
-
+      % blind the study for QC
+      [tmpstudy_oldcohort_blinded]=Blind_OldCohort_XR_Study(tmpid,tmpstudy,accnum_qc);
 
     elseif(isempty(chk_oldcohort) && ~isempty(chk_newcohort))
       %% NEW cohort participant, also blind for screening
+
+      % iterate the accession number counter for QC
+      accnum_qc = accnum_qc+1;
+
+      % blind the study
+      [tmpstudy_oldcohort_blinded]=Blind_OldCohort_XR_Study(tmpid,tmpstudy,accnum_qc);
+
+      % iterate the accession number counter for screening
+      accnum_sc = accnum_sc+1;
+
+      % blind the study for screening
+      [tmpstudy_newcohort_blinded]=Blind_NewCohort_XR_Study(tmpid,tmpstudy,accnum_qc);
 
 
     end %blinding by cohort
@@ -218,6 +93,7 @@ if(size(x_unprocessed,1)>0)
   end %ix
 
   %% save accession number counters
+  UploadToMDB(mdbf,'tblAccNum',{'QC','Screening'},{accnum_qc, accnum_sc}});
 
   %% save .mat file
   save(savef);
