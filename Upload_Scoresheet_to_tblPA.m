376cdc64e8615fe3440ebccf24f184b2dd9a3527
FelixLiu-SF <felixkat@gmail.com>
2018-03-03 11:24:36 -0800

    updated PA results code to upload adjudication results

--- Upload_Scoresheet_to_tblPA.m
+++ Upload_Scoresheet_to_tblPA.m
@@ -11,12 +11,17 @@ disp(horzcat('Uploading scoresheet into database: ',mdbf));
 [x1,f1] = DeployMDBquery(master_mdbf,'SELECT * FROM tblScreening_PA');
 master_id =    indcfind(f1,'^READINGID$','regexpi');
 
+[x2,f2] = DeployMDBquery(master_mdbf,'SELECT * FROM tblSendAdj');
+col_adjid =    indcfind(f2,'^PatientID$','regexpi');
+col_adjsend =  indcfind(f2,'^Send_flag$','regexpi');
+
 %% collect and upload data to mdb scoresheet
 [xS,fS] = DeployMDBquery(mdbf,'SELECT * FROM tblScores');
 
 col_edate = indcfind(fS,'^E_DATE$','regexpi');
 col_dvd =   indcfind(fS,'^DVD$','regexpi');
 col_id =    indcfind(fS,'^READINGID$','regexpi');
+col_reader = indcfind(fS,'^READER$','regexpi');
 
 col_tfrt = indcfind(fS,'^V1TFKLG_R$','regexpi');
 col_pfrt = indcfind(fS,'^V1PFKLG_R$','regexpi');
@@ -32,6 +37,10 @@ chk_read_all = indcfind(xS(:,indcfind(fS,'^E_DATE$','regexpi')),'','empty');
 [conn] = DeployMSAccessConn(master_mdbf);
 
 for jx=1:size(xS,1)
+
+  tmpid = xS(jx,col_id);
+  disp(' ');
+  disp(tmpid;)
 
     % check if signed by reader
     chk_read = xS{jx,col_edate};
@@ -52,10 +61,9 @@ for jx=1:size(xS,1)
             xS{jx,col_DF} = 0;
         end
 
-        disp(xS(jx,col_id));
-        
+
         % check if already uploaded
-        kx = intersect(xS(jx,col_id),x1(:,master_id));
+        kx = intersect(tmpid,x1(:,master_id));
 
         if(isempty(kx))
 
@@ -64,14 +72,34 @@ for jx=1:size(xS,1)
 
         else
             disp('The ID above is already uploaded.');
+
+            % check if this is an adjudication result
+            ax = intersect(tmpid,x2(:,col_adjid));
+            if(~isempty(ax))
+
+              % ID is in the Adj table, check if it was actually sent for adj
+              chk_adj_send = x2{ax(1),col_adjsend};
+              if(chk_adj_send>eps)
+                % yes it was sent for adj, this is probably the adj results
+
+                disp('These are adjudication results, uploading as final scores.')
+                
+                % change reader name to adjudication
+                xS{jx,col_reader} = 'adjudication';
+
+                % insert the results into tblScreening_DF for online report
+                fastinsert(conn,'tblScreening_DF',fS(2:end)',xS(jx,2:end)); pause(0.2);
+
+              end
+            end
+
         end
 
     else % not signed, skip record for now
 
-        disp(xS(jx,col_id));
         disp('The record above is not signed.');
 
-    end        
+    end
 
 end
 
@@ -80,4 +108,3 @@ close(conn);
 if(isempty(chk_read_all))
     movefile(mdbf,destf);
 end
-        
